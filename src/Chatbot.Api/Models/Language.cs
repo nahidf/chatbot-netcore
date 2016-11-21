3f6c71b644a4b51e6905f4eaa150e070a5013895
nahidf <nahid.f@gmail.com>
2016-11-21 01:07:20 -0500

    add the solution and api project

---
+++ src/Chatbot.Api/Models/Language.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chatbot.Api.Models
+{
+    public class Language
+    {
+    }
+}
---
+++ src/Chatbot.Api/Models/TalkModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chatbot.Api.Models
+{
+    public class TalkModel
+    {
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string Language { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
